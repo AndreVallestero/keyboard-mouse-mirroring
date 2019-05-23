e02ad8ff01568c95da55ee69547683df8dad9e0a
Andre Vallestero <andrevallestero@gmail.com>
2019-05-22 20:56:31 -0400

    Use UDP, removed win32api dependency, toggleable

--- client.py
+++ client.py
@@ -3,54 +3,54 @@
 # VK code reference: https://docs.microsoft.com/en-us/windows/desktop/inputdev/virtual-key-codes
 
 import socket
-import win32api
+from ctypes import windll
 
 # Config variables
+serverIp = "192.168.1.2"
 port = 46331
-serverIp = "127.0.0.1"
-
-pollKeys = [0x01, # VK_LBUTTON
-            0x11, # VK_CONTROL
-            0x70, # VK_F1
-            0x71, # VK_F2
-            0x72, # VK_F3
-            0x73, # VK_F4
-            0x74, # VK_F5
-            0x75, # VK_F6
-            0x76] # VK_F7
-
-targetKeys = [0x01, # VK_LBUTTON
+targetKeys = ((0x01, -1), # VK_LBUTTON
             (0x11, 29), # VK_CONTROL
+            (0x31, 2),  # 1
+            (0x32, 3),  # 2
+            (0x33, 4),  # 3
+            (0x34, 5),  # 4
+            (0x35, 6),  # 5
+            (0x36, 7),  # 6
+            (0x37, 8),  # 7
             (0x51, 16), # Q
             (0x57, 17), # W
             (0x45, 18), # E
             (0x52, 19), # R
-            (0x44, 32), # D
-            (0x46, 33), # F
-            (0x31, 2)]  # 1
-
-
-numKeys = len(pollKeys)
-
-oldKeyStates = [0] * numKeys
+            (0x54, 20), # T
+            (0x59, 21)) # Y
 
 # Create socket and connect to server
-s = socket.socket()
-s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-s.connect((serverIp, port))
-print("KMM successfully connected to server")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(b"client connecting", (serverIp, port))
+print("Trying to connect to server")
+s.recv(128)
+print("Connection successfully established with server")
+
+wapi = windll.user32
+numKeys = len(targetKeys)
+oldKeyStates = [0] * numKeys
 
 while True:
-    data = str(s.recv(256).decode("utf8")).split(",")
-
     try:
+        response = s.recv(128)
+        if response == b"z":
+            print("Server heartbeat recieved, connection is alive")
+            continue
+        data = str(response.decode("utf8")).split(",")
+        #print(data)
+        
         # Move mouse
         # Uncomment lines below for different server/client resolutions
         posX = int(data[0]) #* (1366 / 1920)
         posY = int(data[1]) #* (768 / 1080)
         #posX = int(posX)
         #posY = int(posY)
-        win32api.SetCursorPos((posX, posY))
+        wapi.SetCursorPos(posX, posY)
 
         # Press keys
         keyStates = [int(keyState) for keyState in data[2][0:numKeys]]
@@ -60,12 +60,12 @@ while True:
             if action == 1: continue
 
             # Mouse specific handling
-            if pollKeys[i] == 0x01: # Left mouse button offset 2
-                win32api.mouse_event(2 + action, posX, posY, 0, 0)
-            elif pollKeys[i] == 0x02: # Right mouse button offset 8    
-                win32api.mouse_event(8 + action, posX, posY, 0, 0)
+            if targetKeys[i][0] == 0x01: # Left mouse button offset 2
+                wapi.mouse_event(2 + action, posX, posY, 0, 0)
+            elif targetKeys[i][0] == 0x02: # Right mouse button offset 8    
+                wapi.mouse_event(8 + action, posX, posY, 0, 0)
             else:
-                win32api.keybd_event(targetKeys[i][0], targetKeys[i][1], action, 0)
+                wapi.keybd_event(targetKeys[i][0], targetKeys[i][1], action, 0)
         oldKeyStates = keyStates
     except:
         continue
--- server.py
+++ server.py
@@ -1,57 +1,78 @@
 # python 3.6
 
-import socket
-import time
-import win32api
+import socket, time
+from ctypes import windll, Structure, c_long, byref
 
 # Config variables
-pollRate = 60 # How many times per second to check and send mouse and keyboard state
+serverIpv4 = '192.168.1.2' # Local system's ipv4 address to send data from
 port = 46331 # Local port that server will be hosted on
-pollKeys = [0x01, # VK_LBUTTON
+pollRate = 30 # How many times per second to check and send mouse and keyboard state
+mirrorToggleKey = 0x4C # L , The key that will toggle mirroring on or off
+pollKeys = (0x01, # VK_LBUTTON
             0x11, # VK_CONTROL
-            0x70, # VK_F1
-            0x71, # VK_F2
-            0x72, # VK_F3
-            0x73, # VK_F4
-            0x74, # VK_F5
-            0x75, # VK_F6
-            0x76] # VK_F7
+            0x31, # 1
+            0x32, # 2
+            0x33, # 3
+            0x34, # 4
+            0x35, # 5
+            0x36, # 6
+            0x37, # 7
+            0x51, # Q
+            0x57, # W
+            0x45, # E
+            0x52, # R
+            0x54, # Tl
+            0x59) # Y
 
-pollInterval = 1 / pollRate
-pollTime = 0
+class POINT(Structure):
+    _fields_ = [("x", c_long), ("y", c_long)]
 
 # Create and bind socket to port
-s = socket.socket()
-s.bind(('', port))
-s.listen(16) 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((serverIpv4, port))
 print("KMM successfully started and is listening for connections")
 
 # Accept new clients
-client, addr = s.accept()
+data, addr = s.recvfrom(128)
 print("Connection successfully made with client", addr)
+s.sendto(b"connection established", addr)
+
+wapi = windll.user32 # win32api module
+mousePos = POINT()
+pollInterval = 1 / pollRate
+isMirroring = 1
+prevMtkState = 0
+heartbeatTime = 0
 
 while True:
-    # Manage poll timings
-    currTime = time.time()
-    if currTime > pollTime:
-        pollTime = currTime + pollInterval
-        
-        # Gather mouse data
-        posX, posY = win32api.GetCursorPos()
-        mouseData = "{},{},".format(posX, posY)
-
-        # Gather keyboard data
-        keyboardData = ""
-        for key in pollKeys:
-            keyState = win32api.GetKeyState(key) < 0
-            keyboardData += str(int(keyState))
-        
-        # Format data
-        byteData = str(mouseData + keyboardData).encode("utf8")
-        # Send data
-        client.send(byteData)
+    time.sleep(pollInterval)
     
+    # Manage mirroring toggle
+    mtkState = wapi.GetKeyState(mirrorToggleKey) not in (0, 1)
+    if prevMtkState < mtkState:
+        isMirroring = not isMirroring
+        print("Mirroring state =", isMirroring)
+    prevMtkState = mtkState
+    if not isMirroring:
+        currTime = time.time()
+        if heartbeatTime < currTime:
+            print("Sending heartbeat to keep connection alive")
+            s.sendto(b"z", addr)
+            heartbeatTime = currTime + 30
+        continue
+            
+    # Gather mouse data
+    wapi.GetCursorPos(byref(mousePos))
+    mouseData = "{},{},".format(mousePos.x, mousePos.y)
+
+    # Gather keyboard data
+    keyboardData = ""
+    for key in pollKeys:
+        keyState = wapi.GetKeyState(key) not in (0, 1)
+        keyboardData += str(int(keyState))
         
-
-
-    
+    # Format data
+    byteData = str(mouseData + keyboardData).encode("utf8")
+    # Send data
+    #print(byteData)
+    s.sendto(byteData, addr)
